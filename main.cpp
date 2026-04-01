99d262d55025065b07e2ae375ec9626d5d81dc38
xxmxzxfx <xxmxzxfx@gmail.com>
2026-04-01 11:29:15 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,44 +3,48 @@ using namespace std;
 
 int main()
 {   
-    int num1,num2,answer;
-    char opp;
+    int month;
+    cout<<"Enter the number of a month (January is 1) to " << endl << "find out the number days in the month: ";
+    cin >> month;
     
-    cout<<"Enter an operator +,-,/,*"<<endl;
-    cin >> opp;
-    
-        switch (opp) {
-            case '+': cout<<"Enter two numbers to add"<< endl; break;
-            case '-': cout<<"Enter two numbers to subtract"<< endl; break;
-            case '/': cout<<"Enter two numbers to divide"<< endl; break;
-            case '*': cout<<"Enter two numbers to multiply"<< endl; break;
-            default: cout << "Error! enter an operator +,-,/ or *"; break;
-        }
-        
-    cin >> num1;
-    cin >> num2;
-    
-        switch (opp) {
-            case '+':
-                answer = num1 + num2;
-                cout<<"Answer is "<< answer << endl; 
-            break;
-            case '-': 
-                answer = num1 - num2;
-                cout<<"Answer is "<< answer << endl; 
-            break;
-            case '/': 
-                answer = num1 / num2;
-                cout<<"Answer is "<< answer << endl; 
-            break;
-            case '*': 
-                answer = num1 * num2;
-                cout<<"Answer is "<< answer << endl; 
-            break;
-            default: 
-                cout << "ERROR: Enter TWO VALID NUMBERS.";
-            break;
-        }
-            
+    switch (month) {
+        case 1:
+            cout << "January, 31 Days";
+        break;
+        case 2:
+            cout << "February, 28 or 29 Days";
+        break;
+        case 3:
+            cout << "March, 31 Days";
+        break;
+        case 4:
+            cout << "April, 30 Days";
+        break;
+        case 5:
+            cout << "May, 31 Days";
+        break;
+        case 6:
+            cout << "June, 30 Days";
+        break;
+        case 7:
+            cout << "July, 31 Days";
+        break;
+        case 8:
+            cout << "August, 31 Days";
+        break;
+        case 9:
+            cout << "September, 30 Days";
+        break;
+        case 10:
+            cout << "October, 31 Days";
+        break;
+        case 11:
+            cout << "November, 30 Days";
+        break;
+        case 12:
+            cout << "December, 31 Days";
+        break;
+    }
+
     return 0;
 }
